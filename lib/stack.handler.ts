2cfb81ac3e9440bf02667e2182e7f19f648ed7f8
Hugh Evans <hughevans@users.noreply.github.com>
2020-11-03 17:15:47 +1100

    Sendgrid and template

--- lib/stack.handler.ts
+++ lib/stack.handler.ts
@@ -3,10 +3,25 @@ import { DOMParser as dom } from "xmldom";
 import xpath from "xpath";
 import fs from "fs";
 import { Settings, DateTime } from "luxon";
+import sendgrid from "@sendgrid/client";
+import Handlebars from "handlebars";
 
 import { openHours, sunrises, sunsets } from "./data";
 
+declare var process: {
+  env: {
+    SENDGRID_API_KEY: string;
+  };
+};
+
 Settings.defaultZoneName = "Australia/Darwin";
+
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
+
+Handlebars.registerHelper("greaterThan", function (a, b) {
+  var next = arguments[arguments.length - 1];
+  return a > b ? next.fn() : next.inverse();
+});
 
 async function getForecast(filePath: string) {
   const client = new ftp.Client();
@@ -31,6 +46,37 @@ async function readFile(path: string) {
       resolve(data);
     });
   });
+}
+
+async function getTemplate() {
+  const response = await sendgrid.request({
+    method: "GET",
+    url: "/v3/designs/2ae424d3-fb38-4610-a9c5-396ed535191c",
+  });
+  return response[1].html_content;
+}
+
+async function postSingleSend(html_content: String, formattedDate: String) {
+  const response = await sendgrid.request({
+    method: "POST",
+    url: "/v3/marketing/singlesends",
+    body: JSON.stringify({
+      name: `Uluṟu-Kata Tjuṯa daily weather - ${formattedDate}`,
+      send_to: {
+        list_ids: ["d28c612e-a9f2-4b32-992f-5041612ed19f"],
+      },
+      email_config: {
+        editor: "design",
+        generate_plain_content: true,
+        html_content,
+        sender_id: 1119454,
+        subject: `UKTNP weather and track forecast for ${formattedDate}`,
+        suppression_group_id: 15890,
+      },
+    }),
+  });
+
+  return response;
 }
 
 function forecastPath(startTime: String) {
@@ -66,11 +112,6 @@ async function main() {
 
   days.forEach((day) => {
     ["minimum" as const, "maximum" as const].forEach((tempType) => {
-      console.log(
-        `${forecastPath(
-          day.iso
-        )}/element[@type="air_temperature_${tempType}"]/text()`
-      );
       const node = xpath.select(
         `${forecastPath(
           day.iso
@@ -87,7 +128,20 @@ async function main() {
     day.summary = ((node[0] as Node).nodeValue || "").replace(/\.$/, "");
   });
 
-  console.log(days);
+  // console.log(days);
+
+  const template = await getTemplate();
+  const handlebars = Handlebars.compile(template);
+  const html_content = handlebars({ days });
+  // console.log(html_content);
+
+  try {
+    const response = await postSingleSend(html_content, days[0].formatted);
+    console.log(response);
+  } catch (err) {
+    console.log(err);
+    console.log(err.response.body.errors);
+  }
 }
 
 main();
